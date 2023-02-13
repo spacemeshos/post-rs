3f23d323307a11057cf852250f4698227ffe85f2
Dmitry <yashulyak@gmail.com>
2023-02-13 08:53:05 +0200

    add multiple threads

--- benches/aes.rs
+++ benches/aes.rs
@@ -1,4 +1,4 @@
-use std::hint::black_box;
+use std::{hint::black_box, sync::mpsc, thread};
 
 use aes::{
     cipher::{generic_array::GenericArray, BlockEncrypt, KeyInit},
@@ -17,13 +17,32 @@ fn aes_benchmark(c: &mut Criterion) {
     group.throughput(criterion::Throughput::Bytes(data.len() as u64));
     group.bench_function("prove1", |b| {
         b.iter(|| {
-            black_box(prove1(&data, challenge, 0));
+            let (tx, rx) = mpsc::channel();
+            prove1(&data, challenge, 0, tx);
+            black_box(rx)
+        });
+    });
+    group.bench_function("prove4", |b| {
+        b.iter(|| {
+            let (tx, rx) = mpsc::channel();
+            prove_many(4, &data, challenge, 0, tx);
+            black_box(rx);
         });
     });
 }
 
-fn prove1(stream: &[u8], challenge: &[u8; 16], d: u64) -> Vec<usize> {
-    let mut indices = vec![];
+fn prove_many(t: usize, stream: &[u8], challenge: &[u8; 16], d: u64, tx: mpsc::Sender<(u64, u64)>) {
+    thread::scope(|s| {
+        let chunk = stream.len() / t;
+        for i in 0..t {
+            let tx = tx.clone();
+            let stream = &stream[i * chunk..(i + 1) * chunk];
+            s.spawn(|| prove1(stream, challenge, d, tx));
+        }
+    })
+}
+
+fn prove1(stream: &[u8], challenge: &[u8; 16], d: u64, send: mpsc::Sender<(u64, u64)>) {
     let mut output = [0u8; 96];
     let ciphers: Vec<Aes128> = (0..6)
         .map(|i| {
@@ -42,12 +61,14 @@ fn prove1(stream: &[u8], challenge: &[u8; 16], d: u64) -> Vec<usize> {
             let (_, ints, _) = output.align_to::<u32>();
             for j in 0..20 {
                 if ints[j] as u64 <= d {
-                    indices.push(i)
+                    match send.send((j as u64, i as u64)) {
+                        Ok(()) => {}
+                        Err(_) => return,
+                    }
                 }
             }
-        }
+        };
     }
-    indices
 }
 
 criterion_group!(benches, aes_benchmark);
