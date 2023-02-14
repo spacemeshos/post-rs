11364e45801bd2fe235e0bb585cb19c310c91719
Dmitry <yashulyak@gmail.com>
2023-02-14 20:53:51 +0200

    const aes_block

--- src/prove.rs
+++ src/prove.rs
@@ -6,19 +6,20 @@ use std::{sync::mpsc};
 
 pub fn prove(stream: &[u8], challenge: &[u8; 16], d: u64, tx: &mpsc::Sender<(u64, u64)>) {
     const BLOCKS: usize = 6; // number of aes calls per iteration
+    const AES_BLOCK: usize = 16;
     const OUTPUTS: usize = 12; // number of outputs from aes BLOCKS
 
-    let mut output = [0u8; 16 * BLOCKS];
+    let mut output = [0u8; AES_BLOCK * BLOCKS];
     let ciphers: Vec<Aes128> = (0..BLOCKS as u32)
         .map(|i: u32| {
-            let mut key = [0u8; 16];
+            let mut key = [0u8; AES_BLOCK];
             key[..12].copy_from_slice(&challenge[..12]);
             key[12..].copy_from_slice(&i.to_le_bytes());
             Aes128::new(&key.into())
         })
         .collect();
     
-    for (i, chunk) in stream.chunks(16).enumerate() {
+    for (i, chunk) in stream.chunks(AES_BLOCK).enumerate() {
         let labels = chunk.into();
         for (j, cipher) in ciphers.iter().enumerate() {
             cipher.encrypt_block_b2b(labels, (&mut output[j * 16..(j + 1) * 16]).into());
