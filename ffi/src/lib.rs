0d128b840dbbd6a3f84bcf0c7400136f64c2e90f
Bartosz Różański <bartek.roza@gmail.com>
2023-03-14 12:17:27 +0100

    Generate C structs from dependant crates for ffi

--- ffi/src/lib.rs
+++ ffi/src/lib.rs
@@ -5,6 +5,7 @@ use std::{
     path::Path,
 };
 
+pub use post::config::Config;
 use post::prove;
 
 #[repr(C)]
@@ -17,28 +18,9 @@ pub struct ArrayU64 {
 #[repr(C)]
 pub struct Proof {
     nonce: u32,
-    indicies: ArrayU64,
+    indicies: ArrayU8,
     k2_pow: u64,
     k3_pow: u64,
-}
-
-#[repr(C)]
-#[derive(Debug)]
-pub struct Config {
-    pub labels_per_unit: u64,
-    /// K1 specifies the difficulty for a label to be a candidate for a proof.
-    pub k1: u32,
-    /// K2 is the number of labels below the required difficulty required for a proof.
-    pub k2: u32,
-    /// TODO: document
-    pub k2_pow_difficulty: u64,
-    /// TODO: document
-    pub k3_pow_difficulty: u64,
-    /// B is the number of labels used per AES invocation when generating a proof.
-    /// Lower values speed up verification, higher values proof generation.
-    pub b: u32,
-    /// n is the number of nonces to try at the same time.
-    pub n: u32,
 }
 
 /// # Safety
