c4ae3b7ce9199963ce1a9087b910675070757600
Bartosz Różański <bartek.roza@gmail.com>
2023-05-16 11:13:53 +0200

    Simplify logging API

--- ffi/src/log.rs
+++ ffi/src/log.rs
@@ -1,122 +1,46 @@
-use std::{
-    fmt::{Debug, Formatter},
-    mem::ManuallyDrop,
-};
+use std::ffi::c_char;
 
-pub use log::LevelFilter;
 use log::{Level, Log, Metadata, Record};
 
-/// FFI-safe borrowed Rust &str. Can represent `Option<&str>` by setting ptr to null.
+/// FFI-safe borrowed Rust &str
 #[repr(C)]
-pub struct CStr {
-    pub ptr: *const u8,
+pub struct StringView {
+    pub ptr: *const c_char,
     pub len: usize,
 }
 
-impl CStr {
+impl StringView {
     pub unsafe fn to_str<'a>(&self) -> &'a str {
-        let bytes = std::slice::from_raw_parts(self.ptr, self.len);
+        let bytes = std::slice::from_raw_parts(self.ptr as _, self.len);
         std::str::from_utf8_unchecked(bytes)
     }
 }
 
-impl<'a> From<&'a str> for CStr {
+impl<'a> From<&'a str> for StringView {
     fn from(s: &'a str) -> Self {
         Self {
-            ptr: s.as_ptr(),
+            ptr: s.as_ptr() as _,
             len: s.len(),
         }
     }
 }
 
-impl From<Option<&str>> for CStr {
-    fn from(other: Option<&str>) -> Self {
-        if let Some(s) = other {
-            Self::from(s)
-        } else {
-            Self {
-                ptr: std::ptr::null(),
-                len: 0,
-            }
-        }
-    }
-}
-
-/// FFI-safe owned Rust String.
 #[repr(C)]
-pub struct CString {
-    pub ptr: *mut u8,
-    pub cap: usize,
-    pub len: usize,
-}
-
-impl CString {
-    unsafe fn to_str<'a>(&self) -> &'a str {
-        CStr {
-            ptr: self.ptr,
-            len: self.len,
-        }
-        .to_str()
-    }
-}
-
-impl From<String> for CString {
-    fn from(s: String) -> Self {
-        let mut me = ManuallyDrop::new(s);
-        let (ptr, len, cap) = (me.as_mut_ptr(), me.len(), me.capacity());
-        Self { ptr, len, cap }
-    }
-}
-
-impl Drop for CString {
-    fn drop(&mut self) {
-        unsafe {
-            String::from_raw_parts(self.ptr, self.len, self.cap);
-        }
-    }
-}
-
-impl Debug for CString {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        unsafe { self.to_str() }.fmt(f)
-    }
-}
-
-impl Debug for CStr {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        unsafe { self.to_str() }.fmt(f)
-    }
-}
-
-#[repr(C)]
-#[derive(Debug)]
 pub struct ExternCRecord {
     pub level: Level,
-    pub message: CString,  // Preformatted message
-    pub module_path: CStr, // None points to null
-    pub file: CStr,        // None points to null
-    pub line: i64,         // None maps to -1, everything else should fit in u32.
-}
-
-impl<'a> From<&Record<'a>> for ExternCRecord {
-    fn from(record: &Record<'a>) -> Self {
-        Self {
-            level: record.level(),
-            message: CString::from(record.args().to_string()),
-            module_path: CStr::from(record.module_path()),
-            file: CStr::from(record.file()),
-            line: record.line().map(|u| u as i64).unwrap_or(-1_i64),
-        }
-    }
+    pub message: StringView,
+    pub module_path: StringView,
+    pub file: StringView,
+    pub line: i64,
 }
 
 struct ExternCLog {
     callback: extern "C" fn(&ExternCRecord),
-    level: LevelFilter,
+    level: Level,
 }
 
 impl ExternCLog {
-    fn new(level: LevelFilter, callback: extern "C" fn(&ExternCRecord)) -> Self {
+    fn new(level: Level, callback: extern "C" fn(&ExternCRecord)) -> Self {
         Self { level, callback }
     }
 }
@@ -127,7 +51,16 @@ impl Log for ExternCLog {
     }
 
     fn log(&self, record: &Record) {
-        (self.callback)(&ExternCRecord::from(record));
+        let msg = record.args().to_string();
+        let c_record = ExternCRecord {
+            level: record.level(),
+            message: StringView::from(msg.as_str()),
+            module_path: StringView::from(record.module_path().unwrap_or("")),
+            file: StringView::from(record.file().unwrap_or("")),
+            line: record.line().map(|u| u as i64).unwrap_or(-1_i64),
+        };
+
+        (self.callback)(&c_record);
     }
 
     fn flush(&self) {}
@@ -136,11 +69,11 @@ impl Log for ExternCLog {
 /// Set a logging callback function
 #[no_mangle]
 pub extern "C" fn set_logging_callback(
-    level: LevelFilter,
+    level: Level,
     callback: extern "C" fn(&ExternCRecord),
 ) -> i32 {
     match log::set_boxed_logger(Box::new(ExternCLog::new(level, callback)))
-        .map(|()| log::set_max_level(level))
+        .map(|()| log::set_max_level(level.to_level_filter()))
     {
         Ok(_) => 0,
         Err(e) => {
@@ -161,13 +94,10 @@ mod tests {
             assert_eq!(log::Level::Info, record.level);
         }
 
-        super::set_logging_callback(log::LevelFilter::Info, log_cb);
+        super::set_logging_callback(log::Level::Info, log_cb);
         log::info!("Hello, logger");
         log::trace!("Trace log level is disabled");
 
-        assert_eq!(
-            1,
-            super::set_logging_callback(log::LevelFilter::Warn, log_cb)
-        );
+        assert_eq!(1, super::set_logging_callback(log::Level::Warn, log_cb));
     }
 }
