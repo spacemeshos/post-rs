530c4af0cf3c013944a400f7138e423e6d196fb2
Bartosz Różański <bartek.roza@gmail.com>
2023-09-22 11:27:42 +0200

    Refactor and simplify post service code

--- service/src/client.rs
+++ service/src/client.rs
@@ -1,21 +1,60 @@
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::time::Duration;
+
 pub(crate) use spacemesh_v1::post_service_client::PostServiceClient;
-use spacemesh_v1::service_response;
-use spacemesh_v1::ServiceResponse;
-use spacemesh_v1::{GenProofResponse, GenProofStatus, Proof, ProofMetadata};
-use tokio::sync::{mpsc, oneshot};
+use spacemesh_v1::{node_request, service_response};
+use spacemesh_v1::{GenProofResponse, GenProofStatus, Proof, ProofMetadata, ServiceResponse};
+use tokio::sync::mpsc;
+use tokio::time::sleep;
 use tonic::transport::Channel;
 use tonic::Request;
 
-use crate::service::Command;
-use spacemesh_v1::node_request;
+use crate::service::ProofGenState;
 
 pub mod spacemesh_v1 {
     tonic::include_proto!("spacemesh.v1");
 }
 
-pub(crate) async fn run(
+pub(crate) struct ServiceClient {
+    address: String,
+    reconnect_interval: Duration,
+    service: Arc<Mutex<crate::service::PostService>>,
+}
+
+impl ServiceClient {
+    pub(crate) fn new(
+        address: String,
+        reconnect_interval: Duration,
+        service: Arc<Mutex<crate::service::PostService>>,
+    ) -> Self {
+        Self {
+            address,
+            reconnect_interval,
+            service,
+        }
+    }
+
+    pub(crate) async fn run(self) -> eyre::Result<()> {
+        loop {
+            let client = loop {
+                match PostServiceClient::connect(self.address.clone()).await {
+                    Ok(client) => break client,
+                    Err(e) => {
+                        log::info!("could not connect to the node: {e:?}");
+                        sleep(self.reconnect_interval).await;
+                    }
+                }
+            };
+            let res = run(client, self.service.clone()).await;
+            log::info!("client exited: {res:?}");
+        }
+    }
+}
+
+async fn run(
     mut client: PostServiceClient<Channel>,
-    cmds: mpsc::Sender<Command>,
+    service: Arc<Mutex<crate::service::PostService>>,
 ) -> eyre::Result<()> {
     let (tx, mut rx) = mpsc::channel::<ServiceResponse>(1);
     let outbound = async_stream::stream! {
@@ -31,53 +70,34 @@ pub(crate) async fn run(
         log::debug!("Got request from node: {request:?}");
         match request.kind {
             Some(node_request::Kind::GenProof(req)) => {
-                let (cmd_response, rx) = oneshot::channel();
-                let Ok(challenge) = req.challenge.try_into() else {
-                    log::error!("invalid challenge");
-                    tx.send(ServiceResponse {
-                        kind: Some(service_response::Kind::GenProof(GenProofResponse {
-                            status: GenProofStatus::Error as i32,
-                            ..Default::default()
-                        })),
-                    })
-                    .await?;
-                    continue;
-                };
+                let result = service.lock().unwrap().gen_proof(req.challenge);
 
-                // Forward the request to the service
-                cmds.send(Command::GenProof {
-                    challenge,
-                    response: cmd_response,
-                })
-                .await?;
-
-                // Process the response from the service
-                let resp = match rx.await? {
-                    Ok(Some(resp)) => {
+                let resp = match result {
+                    Ok(ProofGenState::Finished { proof, metadata }) => {
                         log::info!("proof generation finished");
                         ServiceResponse {
                             kind: Some(service_response::Kind::GenProof(GenProofResponse {
                                 proof: Some(Proof {
-                                    nonce: resp.0.nonce,
-                                    indices: resp.0.indices.into_owned(),
-                                    pow: resp.0.pow,
+                                    nonce: proof.nonce,
+                                    indices: proof.indices.into_owned(),
+                                    pow: proof.pow,
                                 }),
                                 metadata: Some(ProofMetadata {
-                                    challenge: resp.1.challenge.to_vec(),
+                                    challenge: metadata.challenge.to_vec(),
                                     node_id: Some(spacemesh_v1::SmesherId {
-                                        id: resp.1.node_id.to_vec(),
+                                        id: metadata.node_id.to_vec(),
                                     }),
                                     commitment_atx_id: Some(spacemesh_v1::ActivationId {
-                                        id: resp.1.commitment_atx_id.to_vec(),
+                                        id: metadata.commitment_atx_id.to_vec(),
                                     }),
-                                    num_units: resp.1.num_units,
-                                    labels_per_unit: resp.1.labels_per_unit,
+                                    num_units: metadata.num_units,
+                                    labels_per_unit: metadata.labels_per_unit,
                                 }),
                                 status: GenProofStatus::Ok as i32,
                             })),
                         }
                     }
-                    Ok(None) => {
+                    Ok(ProofGenState::InProgress) => {
                         log::info!("proof generation in progress");
                         ServiceResponse {
                             kind: Some(service_response::Kind::GenProof(GenProofResponse {
