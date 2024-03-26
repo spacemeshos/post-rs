baa04e7113fec8fcbd6085a1e880b33842c93bbb
Bartosz Różański <bartek.roza@gmail.com>
2024-03-26 10:58:27 +0100

    [service] Increase keep-alive interval to 10min

--- service/src/client.rs
+++ service/src/client.rs
@@ -70,7 +70,7 @@ impl<S: PostService> ServiceClient<S> {
     ) -> eyre::Result<Self> {
         let endpoint = Channel::builder(address.parse()?)
             .keep_alive_timeout(Duration::from_secs(20))
-            .http2_keep_alive_interval(Duration::from_secs(60));
+            .http2_keep_alive_interval(Duration::from_secs(10 * 60));
         let endpoint = match tls {
             Some((domain, cert, identity)) => {
                 let domain = match domain {
