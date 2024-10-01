358469934fa609f476173ba224dbb12bdfc563a9
Bartosz Różański <bartek.roza@gmail.com>
2024-10-01 09:59:15 +0200

    fix deprecation warning

--- service/build.rs
+++ service/build.rs
@@ -1,4 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure().compile(&["api/spacemesh/v1/post.proto"], &["api"])?;
+    tonic_build::configure().compile_protos(&["api/spacemesh/v1/post.proto"], &["api"])?;
     Ok(())
 }
